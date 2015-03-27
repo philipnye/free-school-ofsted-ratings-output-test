85160620c0af35dcba60cceea22a9c491431266b
Philip Nye <contact@philipnye.co.uk>
2015-03-27 17:06:07 +0000

    update template code

--- scraper.py
+++ scraper.py
@@ -1,17 +1,6 @@
 
 
 import scraperwiki
-import lxml.html
 
-# Read in a page
-html = scraperwiki.scrape("http://foo.com")
-
-# Find something on the page using css selectors
-root = lxml.html.fromstring(html)
-root.cssselect("div[align='left']")
-
-# Write out to the sqlite database using scraperwiki library
 scraperwiki.sqlite.save(unique_keys=['name'], data={"name": "susan", "occupation": "software developer"})
-
-# An arbitrary query against the database
-scraperwiki.sql.select("* from data where 'name'='peter'")
+print "dogs"
