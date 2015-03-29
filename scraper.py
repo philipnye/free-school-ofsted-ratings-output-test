a48f66efc2fbb67d75a9da30bb13d1801a8c24d5
Philip Nye <philipnye@users.noreply.github.com>
2015-03-29 20:08:51 +0100

    secret variable test

--- scraper.py
+++ scraper.py
@@ -2,7 +2,7 @@ import scraperwiki
 
 scraperwiki.sqlite.save(unique_keys=['name'], data={"name": "susan", "occupation": "software developer"})
 print "dogs"
-if 'MORPH_SECRET'=="test":
+if 'MORPH_SECRET' in os.environ:
   print "woop"
-if 'MORPH_SECRETz':
+if 'MORPH_SECRETz'in os.environ:
   print "hmm"
