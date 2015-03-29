8a50f761fe813954f65cbfdd6b796b361bb2f2ad
Philip Nye <philipnye@users.noreply.github.com>
2015-03-29 20:10:33 +0100

    test secret varibable

--- scraper.py
+++ scraper.py
@@ -4,6 +4,6 @@ import os
 scraperwiki.sqlite.save(unique_keys=['name'], data={"name": "susan", "occupation": "software developer"})
 print "dogs"
 if 'MORPH_SECRET' in os.environ:
-  print 'MORPH_SECRET'
+  print MORPH_SECRET
 if 'MORPH_SECRETz'in os.environ:
   print "hmm"
