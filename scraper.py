487d5470bdd9ec575ee1795ee4fc6598dd49cbfc
Philip Nye <philipnye@users.noreply.github.com>
2015-04-02 07:59:58 +0100

    dirname test

--- scraper.py
+++ scraper.py
@@ -3,11 +3,12 @@
  
 import tweepy, time, sys
 import os
-from os import listdir
 import requests
 
 print os.getcwd()
-print listdir(os.getcwd())
+print os.listdir(os.getcwd())
+print os.path.dirname(os.getcwd())
+print os.path.dirname(os.path.dirname(os.getcwd()))
 
 # if 'MORPH_SECRET' in os.environ:
 # os.environ['MORPH_TWITTER_CONSUMER_KEY']
@@ -25,10 +26,6 @@ ACCESS_SECRET = '1234abcd...'#keep the quotes, replace this with your access tok
 auth = tweepy.OAuthHandler(os.environ['MORPH_TWITTER_CONSUMER_KEY'], os.environ['MORPH_TWITTER_CONSUMER_SECRET'])
 auth.set_access_token(os.environ['MORPH_TWITTER_ACCESS_TOKEN'], os.environ['MORPH_TWITTER_ACCESS_TOKEN_SECRET'])
 api = tweepy.API(auth)
-
-filename2=open(requests.get("https://raw.githubusercontent.com/philipnye/free-school-ofsted-ratings-output-test/master/dummy_tweets.txt"))
-f2=filename2.readlines()
-filename2.close()
 
 for line in f:
     print line
