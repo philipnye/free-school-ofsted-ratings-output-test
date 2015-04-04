9e2239312c4ea5044d09f801454477c0ad646834
Philip Nye <contact@philipnye.co.uk>
2015-04-04 12:16:23 +0100

    fix Tweepy code

--- scraper.py
+++ scraper.py
@@ -16,12 +16,12 @@ print os.path.dirname(os.path.dirname(os.getcwd()))
 # os.environ['MORPH_TWITTER_ACCESS_TOKEN']
 # os.environ['MORPH_TWITTER_ACCESS_TOKEN_SECRET']
  
-auth = tweepy.OAuthHandler(os.environ['MORPH_TWITTER_CONSUMER_KEY'], os.environ['MORPH_TWITTER_CONSUMER_SECRET'])
+auth = tweepy.OAuthHandler(os.environ['MORPH_TWITTER_CONSUMER_KEY'], os.environ['MORPH_TWITTER_CONSUMER_SECRET'], secure=True)
 auth.set_access_token(os.environ['MORPH_TWITTER_ACCESS_TOKEN'], os.environ['MORPH_TWITTER_ACCESS_TOKEN_SECRET'])
 api = tweepy.API(auth)
  
 data = urllib2.urlopen("https://raw.githubusercontent.com/philipnye/free-school-ofsted-ratings-output-test/master/dummy_tweets.txt")
 for line in data:
     print line
-    api.update_status(line)
+    api.update_status(status=line)
     time.sleep(180) #Tweet every 3 minutes
