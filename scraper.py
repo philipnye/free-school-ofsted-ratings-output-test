759a0fccfe16b9b4bea1ca51a1f2d349016a3766
Philip Nye <contact@philipnye.co.uk>
2015-04-12 17:32:18 +0100

    add dummy_tweets read + write test

--- scraper.py
+++ scraper.py
@@ -5,23 +5,24 @@ import tweepy, time, sys
 import os
 import urllib2
 
-print os.getcwd()
-print os.listdir(os.getcwd())
-print os.path.dirname(os.getcwd())
-print os.path.dirname(os.path.dirname(os.getcwd()))
-
-# if 'MORPH_SECRET' in os.environ:
-# os.environ['MORPH_TWITTER_CONSUMER_KEY']
-# os.environ['MORPH_TWITTER_CONSUMER_SECRET']
-# os.environ['MORPH_TWITTER_ACCESS_TOKEN']
-# os.environ['MORPH_TWITTER_ACCESS_TOKEN_SECRET']
- 
 auth = tweepy.OAuthHandler(os.environ['MORPH_TWITTER_CONSUMER_KEY'], os.environ['MORPH_TWITTER_CONSUMER_SECRET'])
 auth.set_access_token(os.environ['MORPH_TWITTER_ACCESS_TOKEN'], os.environ['MORPH_TWITTER_ACCESS_TOKEN_SECRET'])
 api = tweepy.API(auth)
  
-data = urllib2.urlopen("https://raw.githubusercontent.com/philipnye/free-school-ofsted-ratings-output-test/master/dummy_tweets.txt")
-for line in data:
-    print line
-    api.update_status(status=line)
-    time.sleep(180) #Tweet every 3 minutes
+# data = urllib2.urlopen("https://raw.githubusercontent.com/philipnye/free-school-ofsted-ratings-output-test/master/dummy_tweets.txt")
+
+# for line in data:
+#     print line
+#     api.update_status(status=line)
+#     time.sleep(180) #Tweet every 3 minutes
+
+import csv
+with open('https://raw.githubusercontent.com/philipnye/free-school-ofsted-ratings-output-test/master/dummy_tweets.csv', 'rb') as csvfile:
+	dummy_tweet_reader= csv.reader(csvfile, dialect='excel')
+    for row in dummy_tweet_reader:
+        print row
+
+with open('https://raw.githubusercontent.com/philipnye/free-school-ofsted-ratings-output-test/master/dummy_tweets.csv', 'wb') as csvfile2:
+    dummy_tweet_writer = csv.writer(csvfile2)  
+    dummy_tweet_writer.writerow(["5 April 2015"] ["No"]+["Rating"]+ ["New rating - Nottingham University Academy of Science and Technology - requires improvement http://philipnye.github.io/free-school-ofsted-ratings/ http://pic.twitter.com/1LPfQIF7wr"])
+	dummy_tweet_writer.writerow(["5 April 2015"] ["No"]+["Summary"]+ ["Updated - overall 51 out of 77 open, inspected free schools, or 64%, are rated good or better http://philipnye.github.io/free-school-ofsted-ratings/ http://pic.twitter.com/1LPfQIF7wr"])
